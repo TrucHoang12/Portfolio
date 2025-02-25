817b97f331716db4a8ec576a45c3923c3b0281e5
TrucDeThuong <hvthanhtruc121203@gmail.com>
2025-02-25 14:20:04 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("scrollToHome").addEventListener("click", function() {
+    document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("scrollToAboutMe").addEventListener("click", function() {
+    document.getElementById("aboutMeSection").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("scrollToSkills").addEventListener("click", function() {
+    document.getElementById("skillsSection").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("scrollToProjects").addEventListener("click", function() {
+    document.getElementById("projectsSection").scrollIntoView({ behavior: "smooth" });
+});
+
+
+document.getElementById("scrollToContact").addEventListener("click", function() {
+    document.getElementById("contactSection").scrollIntoView({ behavior: "smooth" });
+});
